f31da830bde83a4b9627b0b4a0b4601dbe4ad35c
ymgtech <ymgtech@millenary.net>
2018-02-20 11:26:55 +0900

    [add]StartPanelを最初に表示するように

--- src/treadinorder/MainPanel.java
+++ src/treadinorder/MainPanel.java
@@ -1,3 +1,7 @@
+/*
+ * 各パネル制御用のメインパネル
+ */
+
 package treadinorder;
 
 import java.awt.GridLayout;
@@ -12,6 +16,10 @@ public class MainPanel extends JPanel implements Runnable {
 	public MainPanel() {		
 		// グリッド(行1, 縦1)を作成
 		setLayout(new GridLayout(1, 1));
+		
+		// 最初にStartPanelを表示するように
+		this.sPanel = new StartPanel(this);
+		this.add(sPanel);
 	}
 	
 	public void run() {
--- src/treadinorder/StartPanel.java
+++ src/treadinorder/StartPanel.java
@@ -1,7 +1,15 @@
+/*
+ * スタート画面パネル
+ */
+
 package treadinorder;
 
 import javax.swing.JPanel;
 
 public class StartPanel extends JPanel {
-
+	private MainPanel mp;
+	
+	public StartPanel(MainPanel mp) {
+		this.mp = mp;
+	}
 }
