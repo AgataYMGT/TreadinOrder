95d9b5f41421ba5f6c0e7d5377ba8be3c8785630
ymgtech <ymgtech@millenary.net>
2018-02-24 23:35:29 +0900

    [add]道をタイルに置き換えるための番号付けを行う

--- src/treadinorder/Maze.java
+++ src/treadinorder/Maze.java
@@ -7,8 +7,7 @@ import java.util.Random;
 
 public class Maze {
 	// クラス変数
-	public static final int DUMMY = 0;		// ダミー
-	public static final int ROAD = 1;		// 道
+	public static final int DUMMY = -1;	// ダミー
 	
 	/*
 	 * 上(0), 左(1), 下(2), 右(3)のベクトル
@@ -20,28 +19,38 @@ public class Maze {
 	private int row, col;	// マップの行列
 	private int[][] map;	// 全体のマップ
 	
-	private boolean isReachedTop;
+	private int oneset;	// 指定された順番のワンセットの数
 	
-	public Maze(int row, int col) {
+	private boolean isReachedTop;	// 上面到達のフラグ
+	
+	public Maze(int row, int col, int oneset) {
+		this.oneset = oneset;
+		
 		// row, colが奇数でなければ強制的に奇数に
 		this.row = (row % 2 != 0) ? row : row + 1;
 		this.col = (col % 2 != 0) ? col : col + 1;
-		// 0で初期化の為全てダミーになる
+		// 全てダミーで初期化
 		map = new int[this.row][this.col];
+		for(int i = 0; i < map.length; i++) {
+			for(int j = 0; j < map[i].length; j++) {
+				map[i][j] = DUMMY;
+			}
+		}
 		
 		// 下面から列をランダムに選び道を作り始める
 		isReachedTop = false;
-		makeRoad(this.row - 1, new Random().nextInt(col));
+		makeRoad(this.row - 1, new Random().nextInt(col), 0);
 	}
 	
 	/**
-	 * 再帰的に道を作る。上面に達すると終了する
+	 * 再帰的に番号をつけながら道を作る。上面に達すると終了する
 	 * @param y 作るy座標 
 	 * @param x 作るx座標
+	 * @param roadnum 道につける番号
 	 */
-	public void makeRoad(int y, int x) {
+	public void makeRoad(int y, int x, int roadnum) {
 		// 道を作る
-		map[y][x] = ROAD;
+		map[y][x] = roadnum;
 		if(y == 0) {
 			isReachedTop = true;
 			return;
@@ -57,8 +66,8 @@ public class Maze {
 			if(isReachedTop) return;		// 上面に達していたら処理しない
 			int dy = eachV[0], dx = eachV[1];
 			if( isMakable(y + dy * 2, x + dx * 2) ) {
-				map[y + dy][x + dx] = ROAD;
-				makeRoad(y + dy * 2, x + dx * 2);
+				map[y + dy][x + dx] = (roadnum + 1) % oneset;
+				makeRoad(y + dy * 2, x + dx * 2, (roadnum + 2) % oneset);
 			}
 		}
 	}
